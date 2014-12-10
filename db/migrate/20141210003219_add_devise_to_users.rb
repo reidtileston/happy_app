45949b5dbbfb74738cd86c62687547b7ba60c415
reidtileston <rtilesto@chicagobooth.edu>
2014-12-09 22:21:50 -0600

    Moving Up

--- db/migrate/20141210003219_add_devise_to_users.rb
+++ db/migrate/20141210003219_add_devise_to_users.rb
@@ -1,8 +1,8 @@
 class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
-    change_table(:users) do |t|
+     create_table :users do |t|
       ## Database authenticatable
-      # t.string :email,              null: false, default: ""
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -30,6 +30,8 @@ class AddDeviseToUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :firstname
+      t.string :lastname
 
       # Uncomment below if timestamps were not included in your original model.
       # t.timestamps
