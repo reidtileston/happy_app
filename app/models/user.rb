138dc4d74c756ed7bcc8997aa65a27e4f563526b
reidtileston <rtilesto@chicagobooth.edu>
2014-12-11 13:20:49 -0600

    Revert cb32b01..7a779a1
    This rolls back to commit cb32b01f2693a935b1bc4e6be01be753fbfa82bb.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 has_many :responses
 
-
+validates :firstname, :presence => true
+validates :lastname, :presence => true
 validates :email, :presence => true
 validates :email, :uniqueness => true
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210003219) do
+ActiveRecord::Schema.define(version: 20141210000249) do
 
   create_table "action_plans", force: true do |t|
     t.integer  "user_id"
@@ -47,7 +47,11 @@ ActiveRecord::Schema.define(version: 20141210003219) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -57,8 +61,6 @@ ActiveRecord::Schema.define(version: 20141210003219) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "firstname"
-    t.string   "lastname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
